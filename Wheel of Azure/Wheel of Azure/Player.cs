39a3bc2222904e8a36247f5daae03d523df251be
kcheeeung <k.cheung@berkeley.edu>
2019-09-13 14:07:44 -0700

    add player class

---
+++ Wheel of Azure/Wheel of Azure/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Wheel_of_Azure
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public int Score { get; set; }
+        public int TotalScore { get; set; }
+
+        public Player(string n)
+        {
+            this.Name = n;
+            this.Score = 0;
+            this.TotalScore = 0;
+        }
+    }
+}

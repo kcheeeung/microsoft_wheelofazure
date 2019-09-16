de756fb8fabdf4e2e1a042796304854bb8b78e2a
Brandoncyu <brandoncyu@gmail.com>
2019-09-16 15:37:32 -0700

    merged syntax

--- Wheel of Azure/Wheel of Azure/Program.cs
+++ Wheel of Azure/Wheel of Azure/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -11,62 +12,51 @@ namespace Wheel_of_Azure
 {
     public class Program
     {
+        static string phrase = "MICROSOFT LEAP";
+        //static Wheel wheel = new Wheel();
+        //static Board board = new Board(phrase);
+        static Player playerOne;
+
         static void Main(string[] args)
-
         {   
+            int round_score = 0;
+            int spin_value = 10;
+
+            Console.WriteLine("Please enter your name");
+            string name = Console.ReadLine();
+            playerOne = new Player(name);
+
+            Console.WriteLine("Welcome to Wheel of Azure {0}!", playerOne.Name.ToLower());
+            while (true)
             {
-                int round_score = 0;
-                int spin_value = 10;
-                int count;
-
-
-                string phrase = "MICROSOFT LEAP";
-                Console.WriteLine("Please enter your name");
-                string name = Console.ReadLine();
-                Player One = new Player(name);
-
-                //Wheel wheel = new Wheel();
-                //Board board = new Board(phrase);
-
-
-
-                Console.WriteLine("Welcome to Wheel of Azure {0}!", One.Name.ToUpper());
-
-
-
-
-                while (true)
+                Console.WriteLine("Total Score: {0} ", playerOne.TotalScore);
+                //board.DisplayBoard();
+                bool isNumeric = false;
+                bool isValid = false;
+                int userChoice;
+                do
                 {
-                    Console.WriteLine("Total Score: {0} ", One.TotalScore);
-                    //board.DisplayBoard();
-                    bool isNumeric = false;
-                    bool isValid = false;
-                    int userChoice;
-                    do
+                    Console.WriteLine("Enter 1 to Spin, or 2 to Solve");
+                    string choice = Console.ReadLine();
+                    isNumeric = int.TryParse(choice, out userChoice);
+                    if (userChoice == 1 || userChoice == 2)
                     {
-                        Console.WriteLine("Enter 1 to Spin, or 2 to Solve");
-                        string choice = Console.ReadLine();
-                        isNumeric = int.TryParse(choice, out userChoice);
-                        if (userChoice == 1 || userChoice == 2)
-                        {
-                            isValid = true;
-                        }
-                    } while (!isNumeric || !isValid);
+                        isValid = true;
+                    }
+                } while (!isNumeric || !isValid);
 
 
-                    Console.WriteLine("valid");
-                    //if(userChoice == 1)
-                    //{
-                    //    Spin();
-                    //}
-                    //else
-                    //{
-                    //    Solve();
-                    //}
+                Console.WriteLine("valid");
+                if(userChoice == 1)
+                {
+                    Spin();
                 }
-                Console.WriteLine("You win!");
+                else
+                {
+                    Solve();
+                }
             }
-
+            Console.WriteLine("You win!");
         }
 
         public static void Spin()
@@ -95,7 +85,7 @@ namespace Wheel_of_Azure
             if (pointsEarned > 0)
             {
                 Console.WriteLine("The phrase indeed includes {0}. You won ${1}!", spinGuessLetter, pointsEarned);
-                Player.AddCurrentScore(pointsEarned);
+                playerOne.AddCurrentScore(pointsEarned);
             }
             //if it does not contain the character, it tells the user it has not have it and to guess again.
             //It then goes back to the spin or solve function.
@@ -103,8 +93,6 @@ namespace Wheel_of_Azure
             {
                 Console.WriteLine("The phrase does not include {0}. Try again next turn!", spinGuessLetter);
             }
-
-
         }
 
         public static void Solve()
@@ -114,14 +102,15 @@ namespace Wheel_of_Azure
             Console.Write("Solve the phrase: ");
             string solveGuess = Console.ReadLine().ToLower();
             if (board.MakeGuess(solveGuess) <= 0)
+            {
                 Console.WriteLine("The phrase is not {0}. Please try again", solveGuess);
-            
+            }
         }
 
         public static char SingleLettersOnly(string spinGuess)
         {
             //this function makes sure that a player only puts in a single character. If a player does not, it will continuously ask them until they do.
-            while (spinGuess.Length != 1 || !Regex.IsMatch(spinGuess, @"^[a-z]+$"))
+            while (spinGuess.Length != 1 && !Regex.IsMatch(spinGuess, @"^[a-z]+$"))
             {
                 Console.Write("Type in a single character only, please: ");
                 spinGuess = Console.ReadLine().ToLower();

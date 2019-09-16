0612e0b83a6d436c10271e8a9be96617c9aa5747
Beverly N Ukandu <v-beukan@microsoft.com>
2019-09-16 13:19:47 -0700

    Updated Wheel methods and commented out Program class.

--- Wheel of Azure/Wheel of Azure/Wheel.cs
+++ Wheel of Azure/Wheel of Azure/Wheel.cs
@@ -1,24 +1,81 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Wheel_of_Azure
 {
     public class Wheel
     {
+        public List<int> WheelOfAzure;
+
         public Wheel()
         {
-            // Create data structure with set length of 24 and set numbers
-        }
+            WheelofAzure = new List<int> {
+                2500,
+                600,
+                700,
+                600,
+                650,
+                500,
+                700,
+                0,
+                0,
+                0,
+                0,
+                0,
+                600,
+                550,
+                500,
+                600,
+                650,
+                700,
+                800,
+                500,
+                800,
+                500,
+                650,
+                500
+            };
+        } // end Wheel constructor
 
-        public static int WheelSpin()
-        {
-            // Randomly generate number from wheel & return integer
+            public int WheelSpin()
+            {
+                // Randomly generate number from wheel
+                Random random = new Random();
 
-            return dollarAmount;
-        }
+                int wheelPrizeDollarIndex = random.Next(WheelOfAzure.Count);
+                int wheelPrizeDollarAmount = WheelOfAzure[wheelPrizeDollarIndex];
+
+                return wheelPrizeDollarAmount;
+            } // end WheelSpin method
+
+
+            /* public int[] NewWheel()
+             {
+                 // TODO: Method when multiple rounds+		this	{Wheel_of_Azure.Wheel}	Wheel_of_Azure.Wheel
+
+
+             } // end NewWheel method
+             */
+
+        } // end Wheel class
+
+        //public class Program
+        //{
+        //    static void Main(string[] args)
+        //    {
+        //        Console.WriteLine("testing the link");
+        //        Console.ReadKey();
+        //        Console.WriteLine();
+
+        //        Wheel wheel = new Wheel();
+        //        int prize = wheel.WheelSpin();
+        //        Console.WriteLine(prize);
+
+        //        int prize2 = wheel.WheelSpin();
+        //        Console.WriteLine(prize2);
+        //        Console.ReadKey();
+
+                
+        //    }
+        //}
     }
-}
